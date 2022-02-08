50ee0fa7c9ccd81c497ab07071697d5dfbb536b7
TheodoreBFrazier <theo.frazier@icloud.com>
2022-02-07 22:20:49 -0500

    Set up app.js

--- backend/app.js
+++ backend/app.js
@@ -2,3 +2,15 @@ const cors = require('cors');
 const express = requires('express');
 
 const app = express();
+
+app.use(cors())
+app.use(express.json())
+require("dotenv").config();
+
+app.use("/songs", songsController);
+
+app.get("*", (request, response) => {
+    response.status(404).send("No page found")
+})
+
+module.exports = app;
