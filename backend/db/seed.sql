0a715479f0539fb5e657ae1e50aec87c0196bfcc
TheodoreBFrazier <theo.frazier@icloud.com>
2022-02-09 20:27:11 -0500

    Fixed a synatx issue on seed

--- backend/db/seed.sql
+++ backend/db/seed.sql
@@ -4,8 +4,8 @@ INSERT INTO songs (id, name, artist, album, time, is_favorite)
 VALUES
 ('1', 'The Four Seasons', 'Antonio Vivaldi', 'Classical Music 101', 500, false),
 ('2', 'Oboe Concerto', 'Telemann', 'Oboe Essentials', 300, true),
-('3', 'Cello Dance', 'Yo-Yo Man', 'Bach Essentials', 500, true);
-('4', 'Fur Elise', 'Ludwig Van Beethoven', 'Beethoven', 500, true);
+('3', 'Cello Dance', 'Yo-Yo Man', 'Bach Essentials', 500, true),
+('4', 'Fur Elise', 'Ludwig Van Beethoven', 'Beethoven', 500, true),
 ('5', 'Phantasy Quartet', 'Benjamin Briteen', '20th Century', 600, false);
 
 -- run psql -U postgres -f db/seed.sql
